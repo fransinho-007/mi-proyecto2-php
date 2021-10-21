a0f311faa319b16fb647333d286792106d5dec2d
Andres Vasquez <myapps@myapps.com.bo>
2021-10-20 22:28:16 -0400

    first commit

---
+++ app/Http/Request.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http;
+
+class Request
+{
+    public $url = [];
+    public $controller;
+    public $method;
+
+    function __construct()
+    {
+        $this->url = explode('/', $_SERVER['REQUEST_URI']);
+        $this->setController();
+        $this->setMethod();
+    }
+
+    function setController()
+    {
+        $this->controller = $this->url[3];
+    }
+
+    function getController()
+    {
+        return $this->controller;
+    }
+
+    function setMethod()
+    {
+        $this->method = $this->url[4];
+    }
+
+    function getMethod()
+    {
+        return $this->method;
+    }
+
+    function send()
+    {
+        echo "<p>en el controlador [$this->controller] llamar al metodo [$this->method] </p>";
+    }
+}
